e7e360b7046b377ccadc4ad4e87f50e83946ab8b
RaviKDhandhukiya <rd0302147@gmail.com>
2022-07-11 21:35:15 +0530

    first project

---
+++ tokens/serializers.py
@@ -0,0 +1,16 @@
+
+from attr import field
+from rest_framework import serializers
+from tokens.models import Register
+
+class RegisterSerialiazers(serializers.ModelSerializer):
+    class Meta:
+        model = Register
+        fields = ['id', 'name', 'email', 'address']
+
+        
+
+        
+
+    
+    
